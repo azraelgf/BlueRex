b7057607f7bdc488d287e104ea841f1d2bdb66c1
Oleg <aleh.antukh@gmail.com>
2021-01-07 19:12:31 +0300

    next

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 'use strict'
+
 $(document).ready(function () {
+
+    $(window).on('load', function (){
+        $('.preloader').delay(200).fadeOut('slow', function (){
+            $(this).attr('style', 'display : none !important')
+        });
+    });
     let playBtn = $('#videoPlayBtn');
     $(playBtn).click(function() {
         playBtn.hide();
@@ -10,6 +17,19 @@ $(document).ready(function () {
         $video.attr('src', src + '&autoplay=1');
     });
     baguetteBox.run('.gallery');
+
+    $(window).scroll(function (){
+        if($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        }else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    $('.scrollToTop').click(function (){
+        $('html, body').animate({scrollTop : 0}, 800);
+        return false;
+    });
 });
 
 
